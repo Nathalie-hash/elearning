42ceb16961fba02adfaf796818016bc482688543
Shiro-cha <noum.rzdr@gmail.com>
2022-09-06 23:24:20 +0300

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
             .welcom-text{
                 border-radius:15px;
                 padding:90px 200px;
-                background:#ffffffdf;
+                background:#ffffffbf;
                 color:#202020 !important;
             }
         </style>
