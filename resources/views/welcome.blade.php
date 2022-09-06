36c0addbcdafb62fa328fd0d67f73677fdb538d8
Shiro-cha <noum.rzdr@gmail.com>
2022-09-06 23:26:32 +0300

    message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
 
             .links > a {
                 color: #636b6f;
-                padding: 0 25px;
+                padding: 10 25px;
                 font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
